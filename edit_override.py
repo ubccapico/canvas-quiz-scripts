0745390ade953a6ce269e40093997288d34f4529
Rashi Malhotra <rrashi2000@gmail>
2021-04-16 12:24:28 -0700

    Revert "Fixed bug- if student list contained students already existing in the assignment, program crashed"
    This reverts commit fbeec11a6a46055371959ccb8e450eb3ec6cd033.

--- edit_override.py
+++ edit_override.py
@@ -79,9 +79,8 @@ assignment = course.get_assignment(assignment_id)
 data = assignment.get_overrides()
 
 print("Current Overrides with IDs")
-all_students = []
+
 for element in data:
-    all_students.extend(element.student_ids)
     print(str(len(element.__getattribute__('student_ids')))+ ' students ('+ str(element.__getattribute__('id'))+')')
 
 override_id = input("Enter ID of override to edit: ")
@@ -114,7 +113,7 @@ else:
                 print("Student does not exist: " + str(student))
     else:
         for student in edited_students_list:
-            if student not in curr_students and student not in all_students:
+            if student not in curr_students:
                 curr_students.append(student)
 
     print("Updated list of students:")
