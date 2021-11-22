9b00fb07733a2f718ffa2dcecf00367c6df05fa4
Marko Prodanovic <marko.prodanovic@outlook.com>
2021-11-22 13:35:50 -0800

    Reads token and instance from settings file

--- moderate_students.py
+++ moderate_students.py
@@ -5,34 +5,23 @@ from urllib.error import HTTPError
 from urllib.parse import urlencode
 from urllib.request import Request, urlopen
 import sys
-import add_user_id_col
-
-
-# DATA VARIABLES
-_BASE_URL = "https://ubc.instructure.com/api/v1"
-
-
-with open("token") as t:
-    _TOKEN = t.read()
+import settings
+from canvasapi import Canvas
 
 
 def make_request(url, method="GET", post_fields={}):
+    BASE_URL = settings.INSTANCE + "/api/v1"
     # build request
-    request = Request(
-        "{base_url}/{call_url}".format(base_url=_BASE_URL, call_url=url))
-    request.add_header('Authorization', 'Bearer {token}'.format(token=_TOKEN))
+    request = Request("{base_url}/{call_url}".format(base_url=BASE_URL, call_url=url))
+    request.add_header("Authorization", "Bearer {token}".format(token=settings.TOKEN))
     request.method = method
     if post_fields:
         request.data = urlencode(post_fields).encode()
 
     # open request
-     # open request
     try:
         response = urlopen(request)
-        # logging.debug("HTTP {code} {reason}".format(
-        # code=response.code, reason=response.reason, url=request.full_url))
     except HTTPError as e:
-        # logging.error("{}".format(e))
         return
 
     # decode response
@@ -43,60 +32,78 @@ def make_request(url, method="GET", post_fields={}):
 
 # Functions:
 
+
 def get_quizzes(course_id):
-    response = make_request(
-        "courses/{course_id}/quizzes".format(course_id=course_id))
+    response = make_request("courses/{course_id}/quizzes".format(course_id=course_id))
     print("List of quizzes:")
     for quiz in response:
-        quiz_ids.append(quiz['id'])
-        print("") #line break
-        print(quiz['title'])
-        print("Quiz ID: " + str(quiz['id']) + ", " + "Time limit: "+ str(quiz['time_limit']))
-        
-
+        quiz_ids.append(quiz["id"])
+        print("")  # line break
+        print(quiz["title"])
+        print(
+            "Quiz ID: "
+            + str(quiz["id"])
+            + ", "
+            + "Time limit: "
+            + str(quiz["time_limit"])
+        )
 
 
 def moderate(quiz_id, student_ids, time, choice):
     quiz = make_request(
-        "courses/{course_id}/quizzes/{quiz_id}".format(course_id=course_id, quiz_id=quiz_id))
-    if(choice == '2'):
-        time = calc_time_multiplicative(quiz['time_limit'])
+        "courses/{course_id}/quizzes/{quiz_id}".format(
+            course_id=course_id, quiz_id=quiz_id
+        )
+    )
+    if choice == "2":
+        time = calc_time_multiplicative(quiz["time_limit"])
 
-    for n, student in enumerate(student_ids):
-        response = make_request("courses/{course_id}/quizzes/{quiz_id}/extensions".format(course_id=course_id, quiz_id=quiz_id), method="POST",
-                                post_fields={"quiz_extensions[][user_id]": student,
-                                             "quiz_extensions[][extra_time]": time[n]})
+    for n, sis_id in enumerate(student_ids):
+        canvas = Canvas(settings.INSTANCE, settings.TOKEN)
+
+        user = canvas.get_user(sis_id, "sis_user_id")
+        user_id = user.id
+
+        response = make_request(
+            "courses/{course_id}/quizzes/{quiz_id}/extensions".format(
+                course_id=course_id, quiz_id=quiz_id
+            ),
+            method="POST",
+            post_fields={
+                "quiz_extensions[][user_id]": user_id,
+                "quiz_extensions[][extra_time]": time[n],
+            },
+        )
 
 
 def calc_time_multiplicative(time_limit):
-    return [n*time_limit - time_limit for n in time]
+    return [n * time_limit - time_limit for n in time]
 
 
-if (len(sys.argv) > 1):
+if len(sys.argv) > 1:
     input_file = sys.argv[1]
 else:
-    print("Please run the program again with the csv file passed as command-line arguments")
+    print(
+        "Please run the program again with the csv file passed as command-line arguments"
+    )
     sys.exit()
 
 course_id = input("Enter course ID:")
-add_user_id_col.add_canvas_ids(input_file, course_id)
 
 quiz_ids = []
 get_quizzes(course_id)
 
-user_quiz_ids = input(
-    "Enter quiz ids to moderate(seperate by space) to moderate: ")
+user_quiz_ids = input("Enter quiz ids to moderate(seperate by space) to moderate: ")
 
 
 # READ DATA
 df = pd.read_csv(input_file)
 
-student_ids = (df['ID']).tolist()
-time = df['time'].tolist()
+student_ids = df["sis_id"].tolist()
+time = df["time"].tolist()
 
-choice = input(
-    "Enter 1 if time in file is additive or 2 if it is multiplicative:")
-if(choice == '1' or choice == '2'):
+choice = input("Enter 1 if time in file is additive or 2 if it is multiplicative:")
+if choice == "1" or choice == "2":
     quiz_ids = list(map(int, user_quiz_ids.split()))
     for quiz_id in quiz_ids:
         moderate(quiz_id, student_ids, time, choice)
